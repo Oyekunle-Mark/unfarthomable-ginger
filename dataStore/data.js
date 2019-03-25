bfd7e55f122aeb80efa557a6f40692654c367a33
Oyekunle-Mark <oyekunlemac@gmail.com>
2019-03-25 23:25:15 +0100

    The default data

---
+++ dataStore/data.js
@@ -0,0 +1,21 @@
+const data = {
+  colors: [
+    {
+      id: '8658c1d0-9eda-4a90-95e1-8001e8eb6036',
+      title: 'Ocean Blue',
+      color: '#0070ff',
+      rating: 3,
+      timestamp: 'Sat Mar 12 2016 16:12:09 GMT-0800 (PST)'
+    },
+    {
+      id: 'f9005b4e-975e-433d-a646-79df172e1dbb',
+      title: 'Tomato',
+      color: '#d10012',
+      rating: 2,
+      timestamp: 'Fri Mar 11 2016 12:00:00 GMT-0800 (PST)'
+    }
+  ],
+  sort: 'SORTED_BY_DATE'
+};
+
+export default data;
