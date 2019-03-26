d672be1cec17652edd1e8632386cb87a080083fa
Oyekunle-Mark <oyekunlemac@gmail.com>
2019-03-26 18:31:45 +0100

    the sort reducer

---
+++ reducers/sort.js
@@ -0,0 +1,12 @@
+import C from '../constants/constants';
+
+const sort = (state = 'SORTED_BY_DATE', action) => {
+  switch (action.type) {
+    case C.SORT_COLORS:
+      return action.sortBy;
+    default:
+      return state;
+  }
+};
+
+export default sort;
