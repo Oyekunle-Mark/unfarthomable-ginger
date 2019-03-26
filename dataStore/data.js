60b5181e36cd0453b9cb6a33785204fcb77ed0b6
Oyekunle-Mark <oyekunlemac@gmail.com>
2019-03-26 18:34:15 +0100

    Created the redux store

--- dataStore/data.js
+++ dataStore/data.js
@@ -13,6 +13,13 @@ const data = {
       color: '#d10012',
       rating: 2,
       timestamp: 'Fri Mar 11 2016 12:00:00 GMT-0800 (PST)'
+    },
+    {
+      id: '3315e1p5-3abl-0p523-30e4-8001l8yf4457',
+      title: 'Crazy Green',
+      color: '#00FF00',
+      rating: 0,
+      timestamp: 'Fri Mar 11 2016 12:00:00 GMT-0800 (PST)'
     }
   ],
   sort: 'SORTED_BY_DATE'
