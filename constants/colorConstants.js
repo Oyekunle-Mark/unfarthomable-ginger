3a7d1b248b28f77cf5ebe1fe21beda809349db8b
Oyekunle-Mark <oyekunlemac@gmail.com>
2019-03-26 18:30:27 +0100

    These constants help to perform operations using hex values

---
+++ constants/colorConstants.js
@@ -0,0 +1,11 @@
+// this constant provides some basic colors and their hex values
+
+const colors = {
+  black: '#000000',
+  white: '#FFFFFF',
+  red: '#FF0000',
+  green: '#00FF00',
+  blue: '#0000FF'
+};
+
+export default colors;
