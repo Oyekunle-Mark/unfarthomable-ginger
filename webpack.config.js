ef223fbd2c1420ae68c54e9bf11428c5400ab704
oyekunle-mark <oyekunlemac@gmail.com>
2019-04-26 18:25:44 +0100

    Used the webpack-node-externals library in the webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 
+const nodeExternals = require('webpack-node-externals');
+
 module.exports = {
   mode: 'development',
   target: 'node',
@@ -7,5 +9,6 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  externals: [nodeExternals()]
 };
